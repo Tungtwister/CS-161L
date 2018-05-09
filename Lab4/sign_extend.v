797a17ce321b4a9c7c9a1d0f4819da493d97c1da
Austin Tung <atung003@ucr.edu>
2018-05-08 18:24:37 -0700

    Update sign_extend.v

--- Lab4/sign_extend.v
+++ Lab4/sign_extend.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    19:11:59 06/01/2015 
-// Design Name: 
-// Module Name:    Sign_Extend 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module sign_extend(
     data_in,
     data_out
